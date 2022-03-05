236d0dd2062149d5084ec3bcc4e0f82273334558
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2022-03-05 12:25:34 -0500

    Update counter.js

--- Aisle/counter.js
+++ Aisle/counter.js
@@ -3,6 +3,9 @@ let plus = document.getElementById("up");
 let count = document.getElementById("quantity-box");
 let data = 0;
 let conf = false;
+let price = document.getElementById("price");
+var numberString = price.textContent;
+var x = Number(numberString.replace(/[^0-9\.]+/g,""));
 
 function counterOfMinus(){
 	if(data<=0){
@@ -19,12 +22,14 @@ function counterOfPlus(){
     	conf = true;
     }
 
-	if(data>=10 & conf===true){
-		window.confirm("You chose the same product 10 times. Are you sure you would like to continue?");
+	if(data>=10 && conf===true){
+		window.confirm("You chose the same product "+ data +" times. Are you sure you would like to continue?");
 		conf = false;
 	}
+
 	data = data + 1;
 	count.value = data;
+	price.textContent= (Math.round(data*x * 100) / 100).toFixed(2);
 }
 
 minus.addEventListener("click",counterOfMinus);
