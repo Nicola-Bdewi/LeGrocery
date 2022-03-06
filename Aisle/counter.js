92870870a7b75db1397d450176aad66db3004028
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2022-03-06 11:58:08 -0500

    Update counter.js

--- Aisle/counter.js
+++ Aisle/counter.js
@@ -1,23 +1,30 @@
 let minus = document.getElementById("down");
 let plus = document.getElementById("up");
 let count = document.getElementById("quantity-box");
-let data = 0;
+let data = new Number(localStorage.getItem("counter")); // for not losing the counts after refreshing and pressing + or -
 let conf = false;
 let price = document.getElementById("price");
 var numberString = price.textContent;
 var x = Number(numberString.replace(/[^0-9\.]+/g,""));
+let resetValues = document.getElementById("resetAll");
+
 
 function counterOfMinus(){
+    // Controlling the - button when clicking.
+
 	if(data<=0){
 		window.confirm("Number of items cannot be less than 0!");
 		data++;
 	}
 	data = data - 1;
 	count.value = data;
-	price.textContent= (Math.round(data*x * 100) / 100).toFixed(2);
+	localStorage.setItem("counter",data);
+	localStorage.setItem("price", (Math.round(data*x * 100) / 100).toFixed(2));
+	price.textContent = (Math.round(data*x * 100) / 100).toFixed(2);
 }
 
 function counterOfPlus(){
+	// Controlling the + button when clicking.
 
     if(data%10===0){
     	conf = true;
@@ -30,8 +37,30 @@ function counterOfPlus(){
 
 	data = data + 1;
 	count.value = data;
-	price.textContent= (Math.round(data*x * 100) / 100).toFixed(2);
+	localStorage.setItem("counter",data);
+	localStorage.setItem("price", (Math.round(data*x * 100)/100).toFixed(2)); // The price should br just two decimals after the point.
+    price.textContent = (Math.round(data*x*100) / 100).toFixed(2);
+
+}
+
+price.textContent=localStorage.getItem("price");
+count.value = localStorage.getItem("counter");
+
+function resetEverything(){
+
+/* 
+	This function delete the localstorage.
+
+*/
+	window.localStorage.clear();
+	count.value = localStorage.setItem("counter",1);
+	price.textContent=localStorage.setItem("price",(Math.round(data*x*100) / 100).toFixed(2));
+	location.reload();
 }
 
 minus.addEventListener("click",counterOfMinus);
 plus.addEventListener("click",counterOfPlus);
+resetValues.addEventListener("click",resetEverything);
+
+
+
