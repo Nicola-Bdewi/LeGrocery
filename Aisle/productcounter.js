e97881aed94e81ff13150d61424089c10ebaed85
Tasnim Niloy <82184616+Promass@users.noreply.github.com>
2022-04-05 14:50:54 -0400

    New Home Page

--- Aisle/productcounter.js
+++ Aisle/productcounter.js
@@ -1,7 +1,7 @@
 let minus = document.getElementById("minus");
 let plus = document.getElementById("plus");
 let count = document.getElementById("quantity-box");
-let data = new Number(sessionStorage.getItem("counter")); // for not losing the counts after refreshing and pressing + or -
+let data = new Number(localStorage.getItem("counter")); // for not losing the counts after refreshing and pressing + or -
 let conf = false;
 let price = document.getElementById("price");
 var numberString = price.textContent;
@@ -18,8 +18,8 @@ function counterOfMinus(){
 	}
 	data = data - 1;
 	count.value = data;
-	sessionStorage.setItem("counter",data);
-	sessionStorage.setItem("price", (Math.round(data*x * 100) / 100).toFixed(2));
+	localStorage.setItem("counter",data);
+	localStorage.setItem("price", (Math.round(data*x * 100) / 100).toFixed(2));
 	price.textContent = (Math.round(data*x * 100) / 100).toFixed(2);
 }
 
@@ -37,23 +37,23 @@ function counterOfPlus(){
 
 	data = data + 1;
 	count.value = data;
-	sessionStorage.setItem("counter",data);
-	sessionStorage.setItem("price", (Math.round(data*x * 100)/100).toFixed(2)); // The price should br just two decimals after the point.
+	localStorage.setItem("counter",data);
+	localStorage.setItem("price", (Math.round(data*x * 100)/100).toFixed(2)); // The price should br just two decimals after the point.
     price.textContent = (Math.round(data*x*100) / 100).toFixed(2);
 
 }
 
-price.textContent=sessionStorage.getItem("price");
-count.value = sessionStorage.getItem("counter");
+price.textContent=localStorage.getItem("price");
+count.value = localStorage.getItem("counter");
 
 function resetEverything(){
 
 /* 
-	This function delete the sessionStorage.
+	This function delete the localStorage.
 */
-	window.sessionStorage.clear();
-	count.value = sessionStorage.setItem("counter",1);
-	price.textContent=sessionStorage.setItem("price",initialPrice);
+	window.localStorage.clear();
+	count.value = localStorage.setItem("counter",1);
+	price.textContent=localStorage.setItem("price",initialPrice);
 	location.reload();
 }
 
