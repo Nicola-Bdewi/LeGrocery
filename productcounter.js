3116afa15b702dbd081b72dc901ec92fa8896973
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2022-06-05 20:37:14 -0400

    Update productcounter.js

--- productcounter.js
+++ productcounter.js
@@ -1,65 +1,174 @@
 let minus = document.getElementById("minus");
 let plus = document.getElementById("plus");
 let count = document.getElementById("quantity-box");
-let data = new Number(localStorage.getItem("counter")); // for not losing the counts after refreshing and pressing + or -
 let conf = false;
 let price = document.getElementById("price");
 var numberString = price.textContent;
-var x = Number(numberString.replace(/[^0-9\.]+/g,""));
+var x = Number(numberString.replace(/[^0-9\.]+/g, ""));
 var initialPrice = x.toFixed(2);
 let resetValues = document.getElementById("resetAll");
 
+function initprice(pid) {
+	count.value = 1;
+	price.textContent = initialPrice;
+	if (localStorage.getItem((pid + "counter")) > 1){
+		memoryload(pid);
+	}
+}
 
-function counterOfMinus(){
-    // Controlling the - button when clicking.
+function memoryload(pid) {
+	count.value = localStorage.getItem((pid + "counter"));
+	price.textContent = localStorage.getItem((pid + "price"));
+}
 
-	if(data<=1){
+function counterOfMinus(pid) {
+	let data = new Number(localStorage.getItem(pid + "counter"));
+	
+
+	if (data <= 1) {
 		data++;
 	}
 	data = data - 1;
 	count.value = data;
-	localStorage.setItem("counter",data);
-	localStorage.setItem("price", (Math.round(data*x * 100) / 100).toFixed(2));
-	price.textContent = (Math.round(data*x * 100) / 100).toFixed(2);
+	localStorage.setItem(pid + "counter", data);
+	localStorage.setItem(pid + "price", (Math.round(data * x * 100) / 100).toFixed(2));
+	price.textContent = (Math.round(data * x * 100) / 100).toFixed(2);
 }
 
-function counterOfPlus(){
-	// Controlling the + button when clicking.
+function counterOfPlus(pid) {
+	let data = new Number(localStorage.getItem(pid + "counter"));
 
-    if(data%10===0){
-    	conf = true;
-    }
+	if (data % 10 === 0) {
+		conf = true;
+	}
 
-	if(data>=10 && conf===true){
-		window.confirm("You chose the same product "+ data +" times. Are you sure you would like to continue?");
+	if (data >= 10 && conf === true) {
+		window.confirm("You chose the same product " + data + " times. Are you sure you would like to continue?");
 		conf = false;
 	}
 
 	data = data + 1;
 	count.value = data;
-	localStorage.setItem("counter",data);
-	localStorage.setItem("price", (Math.round(data*x * 100)/100).toFixed(2)); // The price should br just two decimals after the point.
-    price.textContent = (Math.round(data*x*100) / 100).toFixed(2);
+	localStorage.setItem(pid + "counter", data);
+	localStorage.setItem(pid + "price", (Math.round(data * x * 100) / 100).toFixed(2)); // The price should br just two decimals after the point.
+	price.textContent = (Math.round(data * x * 100) / 100).toFixed(2);
 
 }
 
-price.textContent=localStorage.getItem("price");
-count.value = localStorage.getItem("counter");
 
-function resetEverything(){
 
-/* 
-	This function delete the localStorage.
-*/
+function resetEverything() {
+
+	/* 
+		This function delete the localStorage.
+	*/
 	window.localStorage.clear();
-	count.value = localStorage.setItem("counter",1);
-	price.textContent=localStorage.setItem("price",initialPrice);
-	location.reload();
+	count.value = localStorage.setItem("counter", 1);
+	price.textContent = localStorage.setItem("price", initialPrice);
 }
 
-minus.addEventListener("click",counterOfMinus);
-plus.addEventListener("click",counterOfPlus);
-resetValues.addEventListener("click",resetEverything);
+resetValues.addEventListener("click", resetEverything);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let minus = document.getElementById("minus");
+// let plus = document.getElementById("plus");
+// let count = document.getElementById("quantity-box");
+// let data = new Number(localStorage.getItem("counter")); // for not losing the counts after refreshing and pressing + or -
+// let conf = false;
+// let price = document.getElementById("price");
+// var numberString = price.textContent;
+// var x = Number(numberString.replace(/[^0-9\.]+/g,""));
+// var initialPrice = x.toFixed(2);
+// let resetValues = document.getElementById("resetAll");
+
+
+// function counterOfMinus(){
+//     // Controlling the - button when clicking.
+
+// 	if(data<=1){
+// 		data++;
+// 	}
+// 	data = data - 1;
+// 	count.value = data;
+// 	localStorage.setItem("counter",data);
+// 	localStorage.setItem("price", (Math.round(data*x * 100) / 100).toFixed(2));
+// 	price.textContent = (Math.round(data*x * 100) / 100).toFixed(2);
+// }
+
+// function counterOfPlus(){
+// 	// Controlling the + button when clicking.
+
+//     if(data%10===0){
+//     	conf = true;
+//     }
+
+// 	if(data>=10 && conf===true){
+// 		window.confirm("You chose the same product "+ data +" times. Are you sure you would like to continue?");
+// 		conf = false;
+// 	}
+
+// 	data = data + 1;
+// 	count.value = data;
+// 	localStorage.setItem("counter",data);
+// 	localStorage.setItem("price", (Math.round(data*x * 100)/100).toFixed(2)); // The price should br just two decimals after the point.
+//     price.textContent = (Math.round(data*x*100) / 100).toFixed(2);
+
+// }
+
+// price.textContent=localStorage.getItem("price");
+// count.value = localStorage.getItem("counter");
+
+// function resetEverything(){
+
+// /* 
+// 	This function delete the localStorage.
+// */
+// 	window.localStorage.clear();
+// 	count.value = localStorage.setItem("counter",1);
+// 	price.textContent=localStorage.setItem("price",initialPrice);
+// 	location.reload();
+// }
+
+// minus.addEventListener("click",counterOfMinus);
+// plus.addEventListener("click",counterOfPlus);
+// resetValues.addEventListener("click",resetEverything);
 
 
 
